83c819f5464d798206a6403145e4c5f2292b5a5b
Madlyn Espinoza <madlynjoa1@icloud.com>
2020-10-09 22:09:09 -0400

    Verifications

--- app/src/main/java/com/androidClass/adrift/RegisterActivity.java
+++ app/src/main/java/com/androidClass/adrift/RegisterActivity.java
@@ -14,6 +14,13 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.basgeekball.awesomevalidation.AwesomeValidation;
+import com.basgeekball.awesomevalidation.utility.custom.SimpleCustomValidation;
+import com.google.common.collect.Range;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 import static com.basgeekball.awesomevalidation.ValidationStyle.BASIC;
 
@@ -48,8 +55,16 @@ public class RegisterActivity extends AppCompatActivity {
         registerRegAct=(Button)findViewById(R.id.registerRegAct);
         loginReAct=(TextView)findViewById(R.id.loginRegAct);
 
+
+        String regexPassword = "(?=.*[a-z])(?=.*[A-Z])(?=.*[\\d])(?=.*[~`!@#\\$%\\^&\\*\\(\\)\\-_\\+=\\{\\}\\[\\]\\|\\;:\"<>,./\\?]).{1,}";
         awesomeValidation.addValidation(RegisterActivity.this, R.id.email, android.util.Patterns.EMAIL_ADDRESS, R.string.err_email);
-        awesomeValidation.addValidation(RegisterActivity.this, R.id.firstName, "[a-zA-Z\\s]{3,}", R.string.err_name);
+        awesomeValidation.addValidation(RegisterActivity.this, R.id.firstName, "[a-zA-Z\\s]{3,30}", R.string.err_name);
+        awesomeValidation.addValidation(RegisterActivity.this, R.id.lastName, "[a-zA-Z\\s]{3,30}", R.string.err_lastName);
+        awesomeValidation.addValidation(RegisterActivity.this, R.id.usernameET, "[a-zA-Z\\s]{3,30}", R.string.err_username);
+        awesomeValidation.addValidation(RegisterActivity.this, R.id.dateOfBirth, "^(0[1-9]|1[012])[-/.](0[1-9]|[12][0-9]|3[01])[-/.](19|20)\\d\\d$" , R.string.err_dateOfBirth);
+        awesomeValidation.addValidation(RegisterActivity.this, R.id.passwordET,regexPassword,R.string.passwordRegAct);
+        awesomeValidation.addValidation(RegisterActivity.this, R.id.confirmPasswordET,regexPassword,R.string.passwordRegAct);
+
 
         registerRegAct.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -65,14 +80,21 @@ public class RegisterActivity extends AppCompatActivity {
                     //validate if our email is correct and if our name has more than 3 letters
                     if (awesomeValidation.validate()) {
                         //get user input to strings
+
                         String nameCheck = firstNameET.getText().toString();
                         String emailCheck = emailET.getText().toString();
+                        String lNameCheck=lastNameET.getText().toString();
+                        String usernameCheck=usernameRegET.getText().toString();
+                        String dateCheck=dateOfBirthET.getText().toString();
+                        String passwordCheck=passwordET.getText().toString();
+                        String confirmPasswCheck=confirmPassET.getText().toString();
+
                         //if email and name our correct than create a login and direct to Login page
                         Toast.makeText(getApplicationContext(), "created", Toast.LENGTH_LONG).show();
                         Intent registerRegIntent = new Intent(RegisterActivity.this, MainActivity.class);
                         startActivity(registerRegIntent);
                     } else {
-                        //if email and name incorect display invalid
+                        //if email and name incorrect display invalid
                         Toast.makeText(getApplicationContext(), "invalid", Toast.LENGTH_LONG).show();
                     }
                 }
@@ -87,6 +109,7 @@ public class RegisterActivity extends AppCompatActivity {
                 startActivity(loginIntent);
             }
         });
+
 
 
 
